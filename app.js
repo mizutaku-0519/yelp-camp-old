06cf40a7d82596699c5ecd17f2e443a638c401bf
mizutaku-0519 <mizutaku.biz@gmail.com>
2024-11-08 01:20:53 +0900

    viewsと基本的なルーティングを追加

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const path = require("path");
 const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/campgrounds", (req, res) => {
+  res.render("campground/index");
+});
 
 app.get("/", (req, res) => {
   res.send("HOME");
